ecf5032b39181829c89c77317f714700f1347bb4
Aleksy Idzikowski <ge87yok@mytum.de>
2024-02-14 14:48:47 +0100

    empty stuff

---
+++ main.c
@@ -0,0 +1,8 @@
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
---
+++ regexjit.h
@@ -0,0 +1,8 @@
+
+#define DynArr(X) X*
+
+typedef struct Node{
+    DynArr(struct Node*) dest;
+    DynArr(char) sym;
+    bool final;
+}Node;
